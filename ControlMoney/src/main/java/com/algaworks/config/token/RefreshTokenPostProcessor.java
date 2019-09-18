0ab1b4953563cc51772c8cf00d170624a5148023
ivonildo-lopes <ivonildolopes.ti@gmail.com>
2019-09-17 21:30:08 -0300

    retirando o refresh_token do body e add no cookie

---
+++ ControlMoney/src/main/java/com/algaworks/config/token/RefreshTokenPostProcessor.java
@@ -0,0 +1,67 @@
+package com.algaworks.config.token;
+
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Classe responsavel por pegar o Refresh Access Token e jogar no Cookie
+ * onde o javascript não tem acesso, deixando a app mais segura
+ */
+
+@ControllerAdvice
+public class RefreshTokenPostProcessor implements ResponseBodyAdvice<OAuth2AccessToken> {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        // só chamar esse INTERCEPTADOR QUANDO O METODO CHAMADO FOR: post localhost:8080/oauth/token
+        return returnType.getMethod().getName().equals("postAccessToken");
+    }
+
+    @Override
+    public OAuth2AccessToken beforeBodyWrite(OAuth2AccessToken body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+
+        String refreshToken = body.getRefreshToken().getValue();
+
+        HttpServletRequest req = ((ServletServerHttpRequest) request).getServletRequest();
+        HttpServletResponse res = ((ServletServerHttpResponse) response).getServletResponse();
+
+        DefaultOAuth2AccessToken token = (DefaultOAuth2AccessToken) body;
+
+        adicionarRefreshTokenNoCookie(refreshToken, req, res);
+
+        removeRefreshTokenDoBody(token);
+
+        return body;
+    }
+
+    private void adicionarRefreshTokenNoCookie(String refreshToken, HttpServletRequest req, HttpServletResponse res) {
+
+        Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
+        refreshTokenCookie.setHttpOnly(true);
+        refreshTokenCookie.setSecure(false); // TODO: mudar para true em produção (https)
+        refreshTokenCookie.setPath(req.getContextPath() + "/oauth/token");
+        refreshTokenCookie.setMaxAge(2592000); // 30 dias
+        res.addCookie(refreshTokenCookie);
+
+
+    }
+
+    private void removeRefreshTokenDoBody(DefaultOAuth2AccessToken token) {
+        token.setRefreshToken(null);
+    }
+
+}
