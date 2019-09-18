d907c2c8832ccceed91e86081e53d8728e464a23
ivonildo-lopes <ivonildolopes.ti@gmail.com>
2019-09-18 00:31:46 -0300

    adicionando o CORS

---
+++ ControlMoney/src/main/java/com/algaworks/config/CorsFilter.java
@@ -0,0 +1,47 @@
+package com.algaworks.config;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter{
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+
+        HttpServletRequest req =  (HttpServletRequest) request;
+        HttpServletResponse res =  (HttpServletResponse) response;
+
+        res.setHeader("Access-Control-Allow-Origin", req.getHeader("Origin"));
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+
+        if("OPTIONS".equalsIgnoreCase(req.getMethod()) && "localhost:8080".equalsIgnoreCase((req.getMethod()))) {
+            res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+            res.setHeader("Access-Control-Allow-Methods", "Authorization, Content-Type, Accept");
+            res.setHeader("Access-Control-Max-Age", "3600");
+            res.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
