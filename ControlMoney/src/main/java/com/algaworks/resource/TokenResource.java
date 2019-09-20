1e8f16dd3da3d545cdc9bf00568f0668eb40095e
ivonildo-lopes <ivonildolopes.ti@gmail.com>
2019-09-20 10:02:21 -0300

    implementando o logout

---
+++ ControlMoney/src/main/java/com/algaworks/resource/TokenResource.java
@@ -0,0 +1,30 @@
+package com.algaworks.resource;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Esse recurso será usado para fazer o logout da aplicação
+ * para isso ele irá tirar o refreshToken do cookie
+ */
+
+@RestController
+@RequestMapping("/tokens")
+public class TokenResource {
+
+    public void revoke(HttpServletRequest request, HttpServletResponse response) {
+        Cookie cookie = new Cookie("refreshToken", null);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false); // TODO: em producao será true
+        cookie.setPath(request.getContextPath() + "/oauth/token");
+        cookie.setMaxAge(0);
+
+        response.addCookie(cookie);
+        response.setStatus(HttpStatus.NO_CONTENT.value());
+    }
+}
